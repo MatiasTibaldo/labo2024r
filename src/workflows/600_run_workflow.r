a67689c98667dcacc97e2f997863c6e165dc75a2
Matias Tibaldo <matias.tiba@gmail.com>
2024-07-26 13:26:58 +0000

    auto commit 1       20240726 132656

--- src/workflows/600_run_workflow.r
+++ src/workflows/600_run_workflow.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/619_workflow_base_f202109_completo.r"
+PARAM <- "src/workflows/619_workflow_base_f202109_2meses_alternados_sin062020.r"
 
 envg <- env()
 

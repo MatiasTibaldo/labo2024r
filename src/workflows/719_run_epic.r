bc108df9622e3d99beee0099eee5879ed110b9b9
Matias Tibaldo <matias.tiba@gmail.com>
2024-08-03 04:05:27 +0000

    Correcciones undersampling_3

--- src/workflows/719_run_epic.r
+++ src/workflows/719_run_epic.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/719_workflow_epic_f202109_semillerio_undersampling.r"
+PARAM <- "src/workflows/719_workflow_epic_f202109_semillerio_undersampling_3.r"
 
 envg <- env()
 
--- src/workflows/719_workflow_epic_f202109_semillerio_undersampling_3.r
+++ src/workflows/719_workflow_epic_f202109_semillerio_undersampling_3.r
@@ -265,15 +265,18 @@ TS_strategy_base9 <- function( pinputexps )
 
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
-  param_local$final_train$training <- c(202107, 202106, 202105, 202104, 202103, 202102,
-    202101, 202012)
-
-
-  param_local$train$training <- c(202104, 202103, 202102, 202101,
-    202012, 202011, 202010)
+  param_local$final_train$training <- c(
+    202107, 202106, 202105, 202102, 202101, 202012,
+    202004, 202003, 202002, 201911, 201910, 201909
+  )
+  
+  
+  param_local$train$training <- c(
+    202104, 202103, 202102, 202011, 202010, 202009,
+    202001, 201912, 201911, 201908, 201907, 201906
+  )
   param_local$train$validation <- c(202106, 202105)
   param_local$train$testing <- c(202107)
-
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersampling

d86deb0066accb6b81fe352e590693ee94cb672b
Matias Tibaldo <matias.tiba@gmail.com>
2024-08-04 00:00:12 +0000

    Nuevo exp con wf-etapas cambiadas

--- src/workflows/719_run_epic.r
+++ src/workflows/719_run_epic.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/719_workflow_epic_f202109_semillerio_undersampling_4.r"
+PARAM <- "src/workflows/719_workflow_epic_f202109_5.r"
 
 envg <- env()
 
